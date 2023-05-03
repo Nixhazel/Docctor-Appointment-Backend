5181dcc8aed4ab57e33278b1dc7d08467c61fdfd
Nixhazel <neolokor1@gmail.com>
2023-05-03 01:21:03 +0100

    trying to host

--- src/app.ts
+++ src/app.ts
@@ -46,15 +46,15 @@ import doctorRouter from './routes/doctorsRoute'
 const app = express();
 
 // view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'jade');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', indexRouter);
 app.use('/users', usersRouter);
